05984ee49059f8888c1c79a76c2a032dfa26bb9d
James Blue <blue.j4@gmail.com>
2017-10-27 22:01:27 -0400

    Spring init

---
+++ morningservice/src/main/java/com/morningservice/morningservice/MorningserviceApplication.java
@@ -0,0 +1,12 @@
+package com.morningservice.morningservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorningserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorningserviceApplication.class, args);
+	}
+}
