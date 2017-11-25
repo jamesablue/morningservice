1f244518c123c22b6355a601708032226ced312a
James Blue <blue.j4@gmail.com>
2017-11-25 00:45:01 -0500

    Allowing Cross Origin

--- morningservice/src/main/java/com/morningservice/morningservice/controllers/MetroController.java
+++ morningservice/src/main/java/com/morningservice/morningservice/controllers/MetroController.java
@@ -4,13 +4,11 @@ import com.morningservice.morningservice.services.MetroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/")
 public class MetroController {
     @Autowired
