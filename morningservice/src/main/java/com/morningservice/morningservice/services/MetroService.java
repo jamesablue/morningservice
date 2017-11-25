b5e0b262159b5ed6fdc47784966372d8dcb7187c
James Blue <blue.j4@gmail.com>
2017-11-25 00:03:06 -0500

    removed unused import

--- morningservice/src/main/java/com/morningservice/morningservice/services/MetroService.java
+++ morningservice/src/main/java/com/morningservice/morningservice/services/MetroService.java
@@ -1,7 +1,6 @@
 package com.morningservice.morningservice.services;
 
 import com.morningservice.morningservice.utilities.TrainTimes;
-import com.sun.javafx.binding.StringFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
